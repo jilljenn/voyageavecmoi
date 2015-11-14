3611b33fde572d11101c790b2c2d0d9933b379b9
Raito Bezarius <masterancpp@gmail.com>
2015-11-14 18:33:46 +0100

    Apply devtools as a enhanced create store

--- frontend/src/core/Store.js
+++ frontend/src/core/Store.js
@@ -1,9 +1,13 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
+import {devTools, persistState} from 'redux-devtools';
 import thunkMiddleware from 'redux-thunk';
 import travelApp from 'reducers/Reducers.js';
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware
+const enhancedCreateStore = compose(
+  applyMiddleware(
+    thunkMiddleware),
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
 )(createStore);
 
-export default createStoreWithMiddleware(travelApp);
+export default enhancedCreateStore(travelApp);
