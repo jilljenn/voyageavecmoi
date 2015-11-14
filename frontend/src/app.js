e158e35ca6d50d69f5b1b2532dbecf61e88ebd51
Raito Bezarius <masterancpp@gmail.com>
2015-11-14 17:16:01 +0100

    Update to React 0.14 and add Redux

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,7 +2,7 @@ import {render} from 'react-dom';
 import Routes from 'core/Routes';
 
 function runApplication() {
-  render(Routes, document.body);
+  render(Routes, document.getElementById('application'));
 }
 
 runApplication();
--- frontend/src/core/Routes.jsx
+++ frontend/src/core/Routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Router, Route, IndexRoute} from 'react-router';
 
 import Application from 'containers/Application.react';
-import Home from 'components/Home.react';
+import Home from 'containers/Home.react';
 
 import createHashHistory from 'history/lib/createBrowserHistory'
 
