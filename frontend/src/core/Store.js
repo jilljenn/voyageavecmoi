47048495b692d66fdd3ef3c7e472705e455ddd29
Raito Bezarius <masterancpp@gmail.com>
2015-11-15 00:24:46 +0100

    Apply the logger as middleware

--- frontend/src/core/Store.js
+++ frontend/src/core/Store.js
@@ -1,10 +1,14 @@
 import {createStore, compose, applyMiddleware} from 'redux';
 import {devTools, persistState} from 'redux-devtools';
 import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
 import travelApp from 'reducers/Reducers.js';
+
+const logger = createLogger();
 
 const enhancedCreateStore = compose(
   applyMiddleware(
+    logger,
     thunkMiddleware),
   devTools(),
   persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
