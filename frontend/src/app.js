f68ec39768c0bcb5b31df9ac9bf862c471178d73
Raito Bezarius <masterancpp@gmail.com>
2015-11-14 18:33:16 +0100

    Provide the store data + dev tools (redux)

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,8 +1,21 @@
+import React from 'react';
 import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import Store from 'core/Store';
 import Routes from 'core/Routes';
 
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
 function runApplication() {
-  render(Routes, document.getElementById('application'));
+  render(
+    <Provider store={Store}>
+      <section>
+        {Routes}
+        <DebugPanel top right bottom>
+          <DevTools store={Store} monitor={LogMonitor} />
+        </DebugPanel>
+      </section>
+    </Provider>, document.getElementById('application'));
 }
 
 runApplication();
