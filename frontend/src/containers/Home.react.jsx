26e9c6159684e06bb6fec9063830b17059d4b8e3
Raito Bezarius <masterancpp@gmail.com>
2015-11-15 00:26:47 +0100

    Stylize the Home container and add filtering

--- frontend/src/containers/Home.react.jsx
+++ frontend/src/containers/Home.react.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
 
 import OfferList from 'components/OfferList.react';
+import OfferFilter from 'components/OfferFilter.react';
 
 import {connect} from 'react-redux';
-import {fetchOffers} from 'actions/OffersActions';
+import {fetchOffers, filterOffer} from 'actions/OffersActions';
+
+import Radium from 'radium';
 
 function mapStateToProps(state) {
   return {
     offers: state.offers.items,
-    isLoading: state.offers.isFetching
+    isLoading: state.offers.isFetching,
+    currentFilter: state.offers.currentFilter
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    fetch: () => dispatch(fetchOffers())
+    fetch: () => dispatch(fetchOffers()),
+    filter: text => dispatch(filterOffer(text))
   }
 }
 
+@Radium
 class Home extends React.Component {
   static propTypes = {
+    dispatch: React.PropTypes.func,
+    fetch: React.PropTypes.func.isRequired,
+    filter: React.PropTypes.func.isRequired,
     offers: React.PropTypes.array.isRequired,
-    dispatch: React.PropTypes.func.isRequired,
-    isLoading: React.PropTypes.bool.isRequired,
-    fetch: React.PropTypes.func.isRequired
+    isLoading: React.PropTypes.bool,
+    currentFilter: React.PropTypes.string
   }
 
   componentWillMount() {
@@ -31,17 +39,29 @@ class Home extends React.Component {
   }
 
   render() {
-    const {dispatch, offers, isLoading} = this.props;
+    const {dispatch, offers, currentFilter, isLoading} = this.props;
 
     return (
-      <article>
-        <h1>Listes des offres de transport:</h1>
+      <article style={styles.article}>
+        <h1 style={styles.title}>Listes des offres de transport:</h1>
+        <OfferFilter onFilterUpdate={text => this.props.filter(text)} />
         <OfferList
           offers={offers}
+          currentFilter={currentFilter}
         />
       </article>
     );
   }
 }
+
+const styles = {
+  article: {
+    display: 'flex',
+    flexFlow: 'column nowrap'
+  },
+  title: {
+    margin: 'auto'
+  }
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
