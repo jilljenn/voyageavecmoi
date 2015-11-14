ecca306d5425c6c7cd5a414f952e54cdeac3de19
Valentin Lorentz <progval@progval.net>
2015-11-14 23:18:11 +0100

    Fix strings.

--- backend/create_database.py
+++ backend/create_database.py
@@ -5,22 +5,22 @@ def bulk_create_db(db_names):
     for db_name in db_names:
         r.db_create(db_name).run(c)
         print ('BDD `{}` créée.'.format(db_name))
-    print ('Toutes les base de données ont été crées.')
+    print ('Toutes les base de données ont été créées.')
 
 def bulk_create_tables(db_name, table_names):
     c = r.connect("localhost", 28015, db=db_name)
     for table in table_names:
         r.table_create(table).run(c)
         print ('Table `{}` créée.'.format(table))
-    print ('Toutes les tables ont été crées')
+    print ('Toutes les tables ont été créées')
 
 db_names = ['voyageavecmoi']
 tables = {
     'voyageavecmoi': ['offers']
 }
-print ('Je vais commencer à créer la DB pour utiliser le backend!')
+print ('Je vais commencer à créer la DB pour utiliser le backend !')
 bulk_create_db(db_names)
 for db_name, tables in tables.items():
     bulk_create_tables(db_name, tables)
 
-print ('Base de données prête!')
+print ('Base de données prête !')
