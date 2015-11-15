ed3a81d59b815c03fefd96eea0d84e9f48bb455e
Raito Bezarius <masterancpp@gmail.com>
2015-11-15 18:55:09 +0100

    Display loading spinner when loading And pass new fetchers / props to their children

--- frontend/src/containers/Home.react.jsx
+++ frontend/src/containers/Home.react.jsx
@@ -4,21 +4,28 @@ import OfferList from 'components/OfferList.react';
 import OfferFilter from 'components/OfferFilter.react';
 
 import {connect} from 'react-redux';
-import {fetchOffers, filterOffer} from 'actions/OffersActions';
+import {fetchOffers, fetchOffersByCity, fetchMoreOffers, filterOffer} from 'actions/OffersActions';
+import {fetchCities} from 'actions/CitiesActions';
 
+import Halogen from 'halogen';
 import Radium from 'radium';
 
 function mapStateToProps(state) {
   return {
     offers: state.offers.items,
-    isLoading: state.offers.isFetching,
+    cities: state.cities.items,
+    isLoading: state.offers.isFetching || state.cities.isFetching,
+    currentCity: state.offers.currentCity,
     currentFilter: state.offers.currentFilter
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    fetch: () => dispatch(fetchOffers()),
+    fetchOffers: () => dispatch(fetchOffers()),
+    fetchCities: () => dispatch(fetchCities()),
+    fetchCity: city => dispatch(fetchOffersByCity(city)),
+    fetchMore: start => dispatch(fetchMoreOffers(start)),
     filter: text => dispatch(filterOffer(text))
   }
 }
@@ -27,28 +34,49 @@ function mapDispatchToProps(dispatch) {
 class Home extends React.Component {
   static propTypes = {
     dispatch: React.PropTypes.func,
-    fetch: React.PropTypes.func.isRequired,
+    fetchOffers: React.PropTypes.func.isRequired,
+    fetchCities: React.PropTypes.func.isRequired,
+    fetchCity: React.PropTypes.func.isRequired,
+    fetchMore: React.PropTypes.func.isRequired,
     filter: React.PropTypes.func.isRequired,
     offers: React.PropTypes.array.isRequired,
+    cities: React.PropTypes.array.isRequired,
     isLoading: React.PropTypes.bool,
+    currentCity: React.PropTypes.string,
     currentFilter: React.PropTypes.string
   }
 
   componentWillMount() {
-    this.props.fetch();
+    this.props.fetchCities();
+    this.props.fetchOffers();
   }
 
   render() {
-    const {dispatch, offers, currentFilter, isLoading} = this.props;
+    const {dispatch, offers, cities,
+      currentFilter, currentCity, isLoading} = this.props;
 
     return (
       <article style={styles.article}>
-        <h1 style={styles.title}>Listes des offres de transport:</h1>
-        <OfferFilter onFilterUpdate={text => this.props.filter(text)} />
-        <OfferList
-          offers={offers}
-          currentFilter={currentFilter}
-        />
+        <h1 style={styles.title}>Listes des offres de transport</h1>
+        {isLoading &&
+          <section style={styles.loader.section}>
+            <Halogen.GridLoader color={styles.loader.color} />
+          </section>
+        }
+        <section>
+          <OfferFilter
+            onFilterUpdate={text => this.props.filter(text)}
+            onCityUpdate={city => this.props.fetchCity(city)}
+            cities={cities}
+            currentCity={currentCity}
+          />
+          <OfferList
+            offers={offers}
+            currentFilter={currentFilter}
+            onLoadMore={() => this.props.fetchMore(offers.length)}
+            isLoading={isLoading}
+          />
+        </section>
       </article>
     );
   }
@@ -57,10 +85,18 @@ class Home extends React.Component {
 const styles = {
   article: {
     display: 'flex',
-    flexFlow: 'column nowrap'
+    flexFlow: 'column nowrap',
+    margin: 'auto'
   },
   title: {
-    margin: 'auto'
+    margin: 'auto auto 20px auto',
+    fontSize: '42px'
+  },
+  loader: {
+    section: {
+      margin: 'auto'
+    },
+    color: '#4DAF7C'
   }
 };
 
