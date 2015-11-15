ca4dbceb286935704c20e6c1ebffd2e3467a35cb
Raito Bezarius <masterancpp@gmail.com>
2015-11-15 18:53:35 +0100

    Stylize more the Offer component

--- frontend/src/components/Offer.react.jsx
+++ frontend/src/components/Offer.react.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import {Card, Button} from 'belle';
 
+import Radium from 'radium';
+
+@Radium
 export default class Offer extends React.Component {
   static propTypes = {
     offer: React.PropTypes.object.isRequired
@@ -10,9 +13,15 @@ export default class Offer extends React.Component {
   render() {
     return (
       <Card>
-        <p>{this.props.offer.text}</p>
-        <Button>Incorrect?</Button>
+        <p style={styles.text}>{this.props.offer.text}</p>
+        <Button primary>Prendre contact?</Button>
       </Card>
     );
+  }
+}
+
+const styles = {
+  text: {
+    maxWidth: '500px'
   }
 }
