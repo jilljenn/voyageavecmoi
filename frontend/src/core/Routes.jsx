ec65e2c433f019549ebde59c9186c98f878193df
Raito Bezarius <masterancpp@gmail.com>
2015-11-15 22:00:40 +0100

    Activate Browser History in production

--- frontend/src/core/Routes.jsx
+++ frontend/src/core/Routes.jsx
@@ -5,15 +5,33 @@ import Application from 'containers/Application.react';
 import Home from 'containers/Home.react';
 import AboutUs from 'components/AboutUs.react';
 
-export default (
-  <Router>
-    <Route path="/"
-      component={Application}>
-      <IndexRoute component={Home} />
-      <Route component={Home}
-        path="home" />
-      <Route component={AboutUs}
-        path="about" />
-    </Route>
-  </Router>
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+
+const routes = (
+  <Route path="/"
+    component={Application}>
+    <IndexRoute component={Home} />
+    <Route component={Home}
+      path="home" />
+    <Route component={AboutUs}
+      path="about" />
+  </Route>
 );
+
+let router = null;
+if (__DEV__) {
+  router = (
+    <Router>
+      {routes}
+    </Router>
+  );
+} else {
+  router = (
+    <Router history={createBrowserHistory()}>
+      {routes}
+    </Router>
+  );
+}
+
+
+export default router;
