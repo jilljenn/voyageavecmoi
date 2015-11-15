16301499277e52573fb46a63dd623a19cfe56c1d
Raito Bezarius <masterancpp@gmail.com>
2015-11-15 18:56:01 +0100

    Add the city reducer

--- frontend/src/reducers/Reducers.js
+++ frontend/src/reducers/Reducers.js
@@ -1,12 +1,13 @@
 import {combineReducers} from 'redux';
-import {FILTER_OFFER, INVALIDATE_OFFERS, REQUEST_OFFERS, RECEIVE_OFFERS} from 'actions/OffersActions';
 
 import _ from 'lodash';
 
 import offers from './OffersReducer.js';
+import cities from './CitiesReducer.js';
 
 const travelApp = combineReducers({
-  offers
+  offers,
+  cities
 });
 
 export default travelApp;
