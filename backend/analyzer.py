1b5b6bb4e1e4dcfaf2bdceb1fedbc3c07499b4ef
Valentin Lorentz <progval@progval.net>
2015-11-16 17:36:54 +0100

    Add station name normalization.

--- backend/analyzer.py
+++ backend/analyzer.py
@@ -33,6 +33,89 @@ line_number_pattern = re.compile(r'^([0-9]{1,3}|[a-z]{1,2})$', re.I)
 
 Transportation = collections.namedtuple('Transportation', 'type line')
 
+
+class shortcuts:
+    """namespace"""
+    remove = lambda L, x: list(filter(lambda i:i not in x, L))
+    replace = lambda L, x, y: [y if i == x else i for i in L]
+    remove_after = lambda L, x: list(itertools.takewhile(lambda i:i not in x, L))
+
+class MultipleReplacer:
+    """Return a callable that replaces all dict keys by the associated
+    value. More efficient than multiple .replace()."""
+    # From https://github.com/ProgVal/Limnoria/blob/master-2015-10-10/src/utils/str.py#L130-L142
+
+    def __init__(self, dict_):
+        self._dict = dict_
+        dict_ = dict([(re.escape(key), val) for key,val in dict_.items()])
+        self._matcher = re.compile('|'.join(dict_.keys()))
+    def __call__(self, s):
+        return self._matcher.sub(lambda m: self._dict[m.group(0)], s)
+accent_replacer = MultipleReplacer({'é': 'e', 'è': 'e', 'ê': 'e', 'à': 'a', 'ù': 'u'})
+
+def normalize_accents(name):
+    """
+    >>> normalize_accents(['père', 'lachaise'])
+    [['pere', 'lachaise']]
+    """
+    return [list(map(accent_replacer, name))]
+
+def normalize_saint(name):
+    """
+    >>> normalize_saint(['saint', 'philibert'])
+    [['philibert'], ['st', 'philibert']]
+    >>> normalize_saint(['st', 'philibert'])
+    [['philibert'], ['st', 'philibert']]
+    """
+    if 'saint' in name or 'st' in name:
+        return [shortcuts.remove(name, {'saint', 'st'}),
+                shortcuts.replace(name, 'saint', 'st')]
+    else:
+        return [name]
+def normalize_conjunction(name):
+    """
+    >>> normalize_conjunction(['ens', 'de', 'lyon'])
+    [['ens'], ['ens', 'lyon'], ['ens', 'de', 'lyon']]
+    >>> normalize_conjunction(['pont', 'de', 'levallois'])
+    [['levallois'], ['pont', 'levallois'], ['pont', 'de', 'levallois']]
+    """
+    common_prefixes = {
+            'pont', 'rue', 'avenue', 'place',
+            }
+    if name == ['charles', 'de', 'gaulle']:
+        return [['de', 'gaulle'], ['charles', 'de', 'gaulle']]
+    elif len(name) > 2 and name[0] in common_prefixes and name[1] == 'de':
+        return [name[2:], [name[0]] + name[2:], name]
+    elif 'de' in name:
+        return [shortcuts.remove_after(name, {'de'}),
+                shortcuts.remove(name, {'de'}),
+                name]
+    else:
+        return [name]
+
+def normalize(name):
+    """Normalizes a station name so we can more easily match them with
+    the database.
+    Returns a list, which contains different possible normalizations, with
+    different accuracies (first more prone to false positives).
+
+    >>> normalize('Bourg   ')
+    ['bourg']
+    >>> normalize('Saint-Philibert')
+    ['philibert', 'st philibert']
+    >>> normalize('Pont de Levallois')
+    ['levallois', 'pont levallois', 'pont de levallois']
+    >>> normalize('ENS de Lyon')
+    ['ens', 'ens lyon', 'ens de lyon']
+    >>> normalize('Père Lachaise')
+    ['pere lachaise']
+    """
+    name = name.replace('-', ' ').lower().split()
+    names = [name]
+    for pred in (normalize_accents, normalize_saint, normalize_conjunction):
+        names = itertools.chain.from_iterable(map(pred, names))
+    return [' '.join(n) for n in names]
+
 def analyze(text):
     """Tries to find localization informations from a text.
 
