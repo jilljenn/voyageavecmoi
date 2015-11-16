809a28f3feba2d01e0a8a34b2f65a4ebddcea081
Raito Bezarius <masterancpp@gmail.com>
2015-11-16 12:10:53 +0100

    Add a fork ribbon on the header

--- frontend/src/containers/Header.react.jsx
+++ frontend/src/containers/Header.react.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import GitHubForkRibbon from 'react-github-fork-ribbon';
+
 export default class Header extends React.Component {
 	render() {
 		return (
-			<header>
+      <header>
+        <GitHubForkRibbon
+          href="//github.com/jilljenn/voyageavecmoi"
+          target="_blank"
+          position="right">
+          Scissionnez-moi sur GitHub!
+        </GitHubForkRibbon>
 			</header>
 		);
 	}
